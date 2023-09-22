5baa080faea5a42cd1a2f945ca79997c38776bf8
Felipe Moura Martins <felipemouramartins3@gmail.com>
2023-09-22 09:47:50 -0300

    Usando setter para atribuir um valor à propriedade privada

--- src/Cliente.php
+++ src/Cliente.php
@@ -2,17 +2,13 @@
  class Cliente {
     // Propriedades (ou atributos)
     public string $nome;
-    public string $email;
-    public string $senha;
-    public array $telefones;
+    private string $email;
+    private string $senha;
 
-    // Método construtor
-    // Permite configurar a forma de inicialização do objeto, exigindo a atribuição de dados no momento de criar o objeto/instância.
-    // O construtor é sempre chamado de forma automática assim que o objeto é criado.
-    public function __construct(string $nome, string $email)
+    // Métodos getters e setters
+    public function setNome(string $nome):void 
     {
-      // Atribuindo os valores dos parâmetros às propriedades. Usamos o $this para acessar as propriedades desta Classe.
       $this->nome = $nome;
-      $this->email = $email;
+   // Propriedade  Parâmetro
     }
  }
