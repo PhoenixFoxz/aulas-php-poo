d8e666551c04f339c494021bbb97095451407aac
Felipe Moura Martins <felipemouramartins3@gmail.com>
2023-09-28 11:54:53 -0300

    Recursos estáticos

--- index.php
+++ index.php
@@ -3,45 +3,45 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Exemplo 6</title>
+    <title>Exemplo 8</title>
 </head>
 <body>
-    <h1>PHP com POO - Exemplo 6</h1>  
+    <h1>PHP com POO - Exemplo 8</h1>  
     <hr>
     <h2>Assuntos abordados:</h2>
     <ul>
-        <li>Polimorfismo</li>
-        <li>Sobreposição de métodos</li>
-        <li>Uso do <code>parent</code> para acessar superclasse</li>
+        <li>Propriedade e métodos estáticos</li>
+        <li>Acesso direto sem necessidade de objetos/instâncias</li>
+        <li>Uso dp <code>self</code> para acesso (dentro da classe) aos recursos estáticos</li>
     </ul>
 
-<?php
+<?php 
 require_once "src/PessoaFisica.php";
-require_once "src/PessoaJuridica.php";
+require_once "src/Utilitarios.php";
 
-$clientePF = new PessoaFisica; 
-$clientePJ = new PessoaJuridica; 
+$cliente1 = new PessoaFisica;
+$cliente1->setNome("Astrogildo");
+$cliente1->setIdade(75);
 
-// Colocar um nome no Cliente PF
-$clientePF->setNome("Fulano");
-$clientePF->setEmail("fulano@gmail.com");
-$clientePF->setIdade(40);
-$clientePF->setCpf("123.456.789-00");
+$cliente2 = new PessoaFisica;
+$cliente2->setNome("Enzo Auditore");
+$cliente2->setIdade(20);
 
-$clientePJ->setNome("Beltrano S/A");
-$clientePJ->setEmail("blablabla@gmail.com");
-$clientePJ->setAnoFundacao(2000);
-$clientePJ->setCnpj("32.088.0001/000.41");
-$clientePJ->setNomeFantasia("Bla Bla Bla Informática")
+// Usando os recursos estáticos
+Utilitarios::obterData();
 ?>
 
-<hr>
+<h2>Atendimentos do dia: <?=Utilitarios::$dataAtual?></h2>
 
-<h2>Saída de dados</h2>
+<h3>Cliente: <?=$cliente1->getNome()?></h3>
+<p>Tipo de atendimento
+    <?=Utilitarios::definirAtendimento($cliente1->getIdade())?>
+</p>
 
-<section>
-    <?=$clientePF->exibirDados()?>
-    <?=$clientePJ->exibirDados()?>
-</section>
+<h3>Cliente: <?=$cliente2->getNome()?></h3>
+<p>Tipo de atendimento
+    <?=Utilitarios::definirAtendimento($cliente2->getIdade())?>
+</p>
+
 </body>
 </html>
---
+++ src/Utilitarios.php
@@ -0,0 +1,18 @@
+<?php
+class Utilitarios {
+    // Propriedade estática
+    public static string $dataAtual;
+
+    // Método estático
+    public static function obterData()
+    {
+        // self::propriedade
+        // Permite o acesso à propriedade estática.
+        self::$dataAtual = date("d/m/Y");
+    }
+
+    public static function definirAtendimento(int $idade):string 
+    {
+        return $idade >= 60 ? "prioridade" : "normal";
+    }
+}
