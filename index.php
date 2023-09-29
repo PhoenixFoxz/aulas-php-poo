0a0f3c2f9e2ff98c8f3127af932d92bd83aff674
Felipe Moura Martins <felipemouramartins3@gmail.com>
2023-09-29 10:32:05 -0300

    Update index.php

--- index.php
+++ index.php
@@ -3,44 +3,29 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Exemplo 9</title>
+    <title>Exemplo 10</title>
 </head>
 <body>
-    <h1>PHP com POO - Exemplo 9</h1>  
+    <h1>PHP com POO - Exemplo 10</h1>  
     <hr>
     <h2>Assuntos abordados:</h2>
     <ul>
-        <li>Namespaces: agrupamento e organização de recursos (classes, funções, constantes)</li>
-        <li>Prevenção de conflitos entre classes de mesmo nome</li>
-        <li>Configurar e usar <code>namespaces</code> e <code>alias</code></li>
+        <li>Composer: gerenciador de dependência PHP</li>
+        <li>Configurar o projeto com Composer usando <code>composer.json</code></li>
+        <li>Autoload de classes evitando muitos requires e/ou require manuais</li>
     </ul>
 
 <?php 
-// Só de fazer o require/importação das classes, já dá erro no servidor devido a terem o mesmo nome.
-
 use Fornecedor\Pagamento;
 use Prestador\Pagamento as PrestadorPagamento;
-
-// Use individual (um para cada classe)
-// use Tabajara\MEI;
-// use Tabajara\PessoaFisica;
-// use Tabajara\PessoaJuridica;
-
-// Use com uma lista de classes
 use Tabajara\{MEI, PessoaFisica as PF, PessoaJuridica as PJ};
 
 require_once "src/fornecedores/Pagamento.php";
 require_once "src/prestadores/Pagamento.php";
+require_once "src/PessoaFisica.php";
+require_once "src/PessoaJuridica.php";
+require_once "src/MEI.php";
 
-// Forma 1 de usar classes com namespaces
-// $pagamentoFornecedor = new Fornecedor\Pagamento;
-// $pagamentoPrestador = new Prestador\Pagamento;
-
-// Forma 2 de usar classes com namespaces
-// use Fornecedor\Pagamento;
-// use Prestador\Pagamento as PrestadorPagamento; // ALIAS (APELIDO)
-// $pagamentoFornecedor = new Pagamento;
-// $pagamentoPrestador = new PrestadorPagamento; // objeto através do alias
 
 $pagamentoFornecedor = new Pagamento;
 $pagamentoPrestador = new PrestadorPagamento;
@@ -52,9 +37,6 @@ $pagamentoPrestador = new PrestadorPagamento;
 <hr>
 
 <?php
-require_once "src/PessoaFisica.php";
-require_once "src/PessoaJuridica.php";
-require_once "src/MEI.php";
 
 $clientePF = new PF;
 $clientePJ = new PJ;
